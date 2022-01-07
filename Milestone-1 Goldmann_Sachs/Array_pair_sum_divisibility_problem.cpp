0807b20c68d4fc476e673b0b19a6cefec1c9db60
hardik24k9 <hardikpachori1383@gmail.com>
2022-01-07 13:03:13 +0530

    Day-1

---
+++ Milestone-1 Goldmann_Sachs/Array_pair_sum_divisibility_problem.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Approach :- 2 using maps
+// Time Complexity :- O(N)
+// Space Complexity :- O(N)
+
+class Solution 
+{
+  public:
+    bool canPair(vector<int> nums, int k) 
+    {
+        int n = nums.size();
+        if(n%2) return false;
+        
+        map<int,int> mp;
+        
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]%k]++;
+        }
+        
+        for(auto l : mp)
+        {
+            int v1 = l.first;
+            int v2 = k - l.first;
+            
+            if(v1 == 0) // corner case
+            {
+                if(mp[v1]%2) return false;
+            }
+            else if(v1 == v2)
+            {
+                if(mp[v1]%2) return false;
+            }
+            else
+            {
+                if(mp[v1] != mp[v2]) return false;
+            }
+        }
+        
+        return true;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,k;
+        cin >> n >> k;
+        
+        vector<int> nums(n);
+        for(int i=0;i<n;i++) cin >> nums[i];
+
+        Solution ob;
+        bool ans = ob.canPair(nums,k);
+
+        if(ans)
+        cout << "True" << endl;
+        else
+        cout << "False" << endl;
+    }
+}
