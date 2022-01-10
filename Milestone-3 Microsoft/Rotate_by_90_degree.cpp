61c6b68956b3afef1533d4aaf9bdaee3513227ef
hardik24k9 <hardikpachori1383@gmail.com>
2022-01-10 23:59:52 +0530

    Day-1

---
+++ Milestone-3 Microsoft/Rotate_by_90_degree.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Approach :- 1 brute force (using extra matrix)
+// Problem :- Rotate In Anti-clockwise direction (so reverse will be on columns)
+// Time Complexity :- O(N*N)
+// Space Complexity :- O(N*N)
+
+void rotate(vector<vector<int> >& matrix)
+{
+    int n = matrix.size();
+    vector<vector<int>> matrix1(n,vector<int> (n));
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            matrix1[n-1-i][j] = matrix[j][i];
+        }
+    }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            matrix[i][j] = matrix1[i][j];
+        }
+    }
+}
+
+// Approach :- 2 taking transpose then reversing all every columns 
+// Problem :- Rotate In Anti-clockwise direction (so reverse will be on columns)
+// Time Complexity :- O(N*N)
+// Space Complexity :- O(1)
+
+void rotate(vector<vector<int> >& matrix)
+{
+    int n = matrix.size();
+    
+    // taking transpose of a matrix
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+    
+    // now just reverse every column as it's in anti-clockwise direction
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n/2;j++)
+        {
+            swap(matrix[j][i],matrix[n-1-j][i]);
+        }
+    }
+}
