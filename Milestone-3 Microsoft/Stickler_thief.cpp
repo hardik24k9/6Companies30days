398fd6e1df490eb31d5a40be33cf211375cee656
hardik24k9 <hardikpachori1383@gmail.com>
2022-01-11 03:03:04 +0530

    Day-1

--- Milestone-3 Microsoft/Stickler_thief.cpp
+++ Milestone-3 Microsoft/Stickler_thief.cpp
@@ -8,6 +8,28 @@ using namespace std;
 // Approach :- 2 using recursion with memoization
 // Time Complexity :- O(N)
 // Space Complexity :- O(N)
+class Solution
+{
+    public:
+    int dp[10001];
+    
+    int solve(int arr[],int i)
+    {
+        if(i <= -1) return 0;
+        if(dp[i] != -1) return dp[i];
+        
+        int op1 = arr[i] + solve(arr,i-2);
+        int op2 = solve(arr,i-1);
+        return dp[i] = max(op1,op2);
+    }
+    
+    int FindMaxSum(int arr[], int n)
+    {
+        memset(dp,-1, sizeof dp);
+        int ans = solve(arr,n-1);
+        return ans;
+    }
+};
 
 // Approach :- 3 using tabulation DP
 // Time Complexity :- O(N)
